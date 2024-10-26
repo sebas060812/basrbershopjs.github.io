eac54f99fedb7e2e6795f0da6a781f87fdf11caa
sebas060812 <gomezmunozj037@gmail.com>
2024-10-26 06:47:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showDetail(corte) {
+    let detalles = {
+        fade: "El corte Fade es ideal para quienes buscan un look moderno y versátil. Se puede personalizar según tus preferencias.",
+        pompadour: "El Pompadour destaca por su volumen y elegancia. Es perfecto para ocasiones especiales o un look diario.",
+        buzz: "El corte Buzz es perfecto para quienes desean un mantenimiento mínimo. Simple, práctico y siempre a la moda.",
+        clasico: "El Corte Clásico nunca pasa de moda y es ideal para un look profesional.",
+        cesar: "El Corte César es ideal para un look casual y fresco, fácil de mantener.",
+        garzon: "El Corte a la Garzón permite mucha versatilidad y es ideal para quienes buscan algo diferente."
+    };
+    alert(detalles[corte]);
+}
